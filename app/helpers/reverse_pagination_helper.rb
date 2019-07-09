172610faadccbd14c73892387e1920d899239c5d
Stanislav Zaleski <sigmaray@gmail.com>
2019-07-09 09:36:46 +0300

    Refactor ReversePagination

--- app/helpers/reverse_pagination_helper.rb
+++ app/helpers/reverse_pagination_helper.rb
@@ -4,37 +4,38 @@ module ReversePaginationHelper
   def reverse_pagination(paginator)
     return if paginator.records.blank?
 
-    pagination_element = content_tag :div, '', :class => 'pagination'
-
-    pagination_element << if !paginator.last_page?
-      link_to('[last]', request.GET.merge(:page => paginator.total_pages))
-    else
-      '[last]'
+    content_tag :div, class: 'pagination' do
+      concat(if !paginator.last_page?
+        link_to('[last]', request.GET.merge(:page => paginator.total_pages))
+      else
+        '[last]'
+      end)
+      concat ('&nbsp;' * 3).html_safe
+      concat(
+        if paginator.page < paginator.total_pages
+          link_to('[<]', request.GET.merge(:page => paginator.page + 1))
+        else
+           '[<]'
+        end
+      )
+      concat ('&nbsp;' * 3).html_safe
+      concat paginator.page.to_s
+      concat ('&nbsp;' * 3).html_safe
+      concat(
+        if !paginator.first_page?      
+          link_to('[>]', request.GET.merge(:page => paginator.page - 1))
+        else
+          '[>]'
+        end
+      )
+      concat ('&nbsp;' * 3).html_safe
+      concat(
+        if !paginator.first_page?
+          link_to('[first]', request.GET.merge(:page => 1))
+        else
+          '[first]'
+        end
+      )
     end
-
-    pagination_element << ('&nbsp;' * 3).html_safe
-    pagination_element << if paginator.page < paginator.total_pages
-      link_to('[<]', request.GET.merge(:page => paginator.page + 1))
-    else
-       '[<]'
-    end
-    pagination_element << ('&nbsp;' * 3).html_safe
-    pagination_element << paginator.page.to_s
-    pagination_element << ('&nbsp;' * 3).html_safe
-    pagination_element << if !paginator.first_page?      
-      link_to('[>]', request.GET.merge(:page => paginator.page - 1))
-    else
-      '[>]'
-    end
-
-    pagination_element << ('&nbsp;' * 3).html_safe
-    pagination_element << if !paginator.first_page?
-      link_to('[first]', request.GET.merge(:page => 1))
-    else
-      '[first]'
-    end
-    
-    pagination_element
   end
 end
-
